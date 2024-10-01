86437253c85b36ea218e6c4f4956eddc93e7f79d
farhannadim311 <121982005+farhannadim311@users.noreply.github.com>
2024-10-01 15:16:48 -0500

    Create binary_search.c

---
+++ binary_search.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+int binary_search(int val, int N, int* A);
+
+int main()
+{
+    int sorted_array[] = {1,2,3,4,5,6,7,8,9,10,11,12};
+    int found = binary_search(10, 11, sorted_array);
+    printf("The index is at %d \n" , found);
+}
+int binary_search(int val, int N, int* A)
+{
+    int m = N/2;
+    int l = 0;
+    int r = N;
+    while (l < r)
+    {
+        if (A[m] == val)
+        {
+            return m;
+        }
+        else if (A[l] > val )
+        {
+           r = m + 1;
+        }
+        else 
+        {
+            l = m + 1;
+        }
+    m = (l + r) / 2;
+    }
+}
